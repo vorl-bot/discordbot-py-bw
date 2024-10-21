c8c1a5d5a0e4ead9e596ae1c8923b2483f8a5e8c
unreal-hw <ennisc21@gmail.com>
2024-10-21 19:23:39 +0900

    Update magnum1.py

--- magnum1.py
+++ magnum1.py
@@ -23,12 +23,12 @@ def belcher():
         text2 = random.choice(['양처럼 생긴 물체가 도망쳤다.','손이 미끄러졌다.'])+' 양털 깎기에 실패했다...'
     
     elif lamb_result > 50 and lamb_result < 99:
-        text1 = '양털을 '+str(random.choice(1,41))+'kg 깎았다.'
+        text1 = '양털을 '+str(random.randrange(1,41))+'kg 깎았다.'
         text2 = random.choice(colors)+'을 띤 퐁당이를 구출했다!'
 
     elif lamb_result >= 99:
         colors = colors+['무지개색','오로라빛']
-        text1 = '희귀한 '+random.choice(['반짝이','무지개색','오로라빛'])+' 양털을'+str(random.choice(1,51))+'kg 깎았다.'
+        text1 = '희귀한 '+random.choice(['반짝이','무지개색','오로라빛'])+' 양털을'+str(random.randrange(1,51))+'kg 깎았다.'
         text2 = random.choice(colors)+'을 띤 퐁당이를 구출했다!'
 
     return text1, text2
