9dbb03e072315947b3f7cb01774a6193af5112ed
unreal-hw <ennisc21@gmail.com>
2024-10-26 19:28:24 +0900

    dd

--- discordbot.py
+++ discordbot.py
@@ -148,7 +148,7 @@ async def on_message(message):
         embed.add_field(name=fishNum+'마리를 잡았다!', value='', inline=True)
         await message.channel.send(embed=embed, reference=message)
 
-     #운송게임
+    #운송게임
     if message.content.startswith(f'{PREFIX}운송'):
         trans = magnum1.transport()
 
@@ -157,6 +157,26 @@ async def on_message(message):
                             color=0x8c2b3f)
         embed.add_field(name='', value='', inline=False)
         await message.channel.send(embed=embed, reference=message)
+
+    #특수가챠
+    if message.content.startswith(f'{PREFIX}특챠'):
+        special = gacha.specialGacha()
+
+        if len(special) == 2:
+            embed=discord.Embed(title="달각달각달각달각······.", 
+                                description="기계 안에서 캡슐이 나왔다! 캡슐을 열어보면·······.", 
+                                color=0x008083)
+            embed.add_field(name="", value=special[0]+"이(가) 나왔다. 인형에 딱 맞을 것 같은 사이즈네!", inline=False)
+            embed.set_image(url=special[1])
+            await message.channel.send(embed=embed, reference=message)
+
+        if len(special) == 1:
+            embed=discord.Embed(title="달각달각달각달각······.", 
+                                description="기계 안에서 캡슐이 나왔다! 캡슐을 열어보면·······.", 
+                                color=0x008083)
+            embed.add_field(name="", value=special+"이(가) 나왔다.", inline=False)
+            await message.channel.send(embed=embed, reference=message)
+
 
 try:
     client.run(TOKEN)
