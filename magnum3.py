23814c939571414349c8dcc0f54e646e40be5923
unreal-hw <ennisc21@gmail.com>
2024-11-18 22:08:34 +0900

    Update magnum3.py

--- magnum3.py
+++ magnum3.py
@@ -17,19 +17,19 @@ def weaving():
     weave_result = random.randrange(1,101)
 
     if weave_result < 51:
-        text1 = '**저가형 옷감**이 완성되었다.'
+        text1 = '저가형 옷감이 완성되었다.'
         text2 = '으음... 어찌저찌 성공했지만, 상당히 엉성한 옷감이다. 이걸로 만든 옷을 입고 싶진 않은걸.'
 
     elif weave_result > 50 and weave_result < 81 :
-        text1 = '**일반 옷감**이 완성되었다.'
+        text1 = '일반 옷감이 완성되었다.'
         text2 = '무난한 옷감이다. 염색해서 옷을 만들어 입어도 괜찮겠네.'
 
     elif weave_result > 80 and weave_result < 96 :
-        text1 = '**고급 옷감**이 완성되었다.'
+        text1 = '고급 옷감이 완성되었다.'
         text2 = '곱고 아름다운 옷감이다. 염색해서 옷을 만들어 입으면 분명 멋질 거야.'
 
     elif weave_result > 95 :
-        text1 = '**최고급 옷감**이 완성되었다.'
+        text1 = '최고급 옷감이 완성되었다.'
         text2 = '아니, 이건?! 옷감에서 광택이 나는 것만 같다! 염색해서 옷을 만들어 입으면 누구라도 안색이 살 거야!'
 
     return text1, text2
@@ -53,3 +53,19 @@ def valuation():
         text = 'S등급'
 
     return text
+
+def TLmission():
+
+    return
+
+def TLitem():
+
+    return
+
+def TLattack():
+
+    return
+
+def TLdefence():
+
+    return
